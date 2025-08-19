2c621ac88a92ef12f7014189ac23a00baba5e485
monesh1999 <r994942@gmail.com>
2025-08-19 15:58:52 +0530

    initial commit

---
+++ src/example/demo/Casting.java
@@ -0,0 +1,20 @@
+package example.demo;
+
+public class Casting {
+	public static void main(String[] args) {
+		
+		
+		int num = 10;
+		double result = num; // Automatic conversion
+		
+		System.out.println(num);
+		System.out.println(result); 
+		
+		double res = 9.88d;
+		int my = (int) res;
+		System.out.println(res);
+		System.out.print(my);
+
+	}
+
+}
