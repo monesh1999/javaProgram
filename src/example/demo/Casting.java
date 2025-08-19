d82198d807513038bf6a3e99432265574c2b5826
monesh1999 <r994942@gmail.com>
2025-08-19 16:28:14 +0530

    initial commit

--- src/example/demo/Casting.java
+++ src/example/demo/Casting.java
@@ -13,7 +13,37 @@ public class Casting {
 		double res = 9.88d;
 		int my = (int) res;
 		System.out.println(res);
-		System.out.print(my);
+		System.out.println(my);
+		
+		byte b =10;
+		short s = 200;
+        long l = 100000L;
+        float f = 3.14f;
+        double d = 99.99;
+        char c = 'A';
+        boolean flag = true;
+		int Myint = 10;
+		//Auto-boxing
+//		The automatic conversion of primitive types to the object of their 
+//		corresponding wrapper classes is known as autoboxing. 
+//		For example: conversion of int to Integer, long to Long, double to Double, etc. 
+//		
+		Double dObj = d;
+		Character cObj = c;
+		Boolean flagObj = flag;
+		Float fObj =f;
+		Long lObj =l;
+		Short sObj = s;
+		Integer num1 = Myint;
+		Byte obj = b;
+		System.out.println(num1);
+		System.out.println(obj);
+		System.out.println(cObj);
+		System.out.println(dObj);
+		System.out.println(flagObj);
+		System.out.println(fObj);
+		System.out.println(lObj);
+		System.out.println(sObj);
 
 	}
 
