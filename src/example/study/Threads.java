3eb1521dcfb39215cc42547dd08359ecfbfc9d4b
monesh1999 <r994942@gmail.com>
2025-09-19 16:13:23 +0530

    intial commit

---
+++ src/example/study/Threads.java
@@ -0,0 +1,45 @@
+package example.study;
+
+
+/*
+ * thread is light weight sub process are parth of execution within a program
+ * 
+ * 
+ * it can creating using or implement using 1,2
+ * init() ,start(),stop(),service --run()
+ * thread -- task (timebound ) ---sleep(ms),wait(),await()
+ * how thread is imlemented
+ * 1.extends thread (class)
+ * 2.implement runnable (interface)
+ * 
+ * 
+ * lifecycle of thread
+ * 
+ * 1.new (constucting) -- starting the thread by intilizing the obj in the class(ex)
+ * 2.runnable (prepared to execute the code in interface)
+ * 3.running (thread for execution with /without time slot)
+ * 4.Blocked (in active)
+ * 5.Dead / terminated(thread exit at the run () process)
+ * 
+ * 
+ * thread prirorities(based on the scenario)
+ * 1-lowest (Thread.Min_Priority)
+ * 5-standard(Thread.NORM_PRIORTY)
+ * 10-highest(Thread.MAX_Priorty)
+ */
+
+public class Threads extends Thread {
+	 public void run()
+	    {
+	      	String str = "Thread is Running Successfully";
+	        System.out.println(str);
+	    }
+
+	public static void main(String[] args) {
+		
+		Threads t = new Threads();
+		
+
+	}
+
+}
